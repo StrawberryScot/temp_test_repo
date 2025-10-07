d32a578b9facce3ffd94aac48aa2d70e4d235ae6
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-10-07 10:01:44 +0100

    test: added tests for present

---
+++ tests/test_present.py
@@ -0,0 +1,23 @@
+from lib.present import *
+import pytest
+
+def test_wrap_and_then_unwrap():
+    present = Present()
+    present.wrap(21)
+    assert present.unwrap() == 33
+
+def test_unwrap_without_wrapping():
+    present = Present()
+    with pytest.raises(Exception) as e:
+        present.unwrap()
+    message = str(e.value)
+    assert message == "No contents have been wrapped."
+
+def test_wrapping_already_wrapped():
+    present = Present()
+    present.wrap(21)
+    with pytest.raises(Exception) as e:
+        present.wrap(32)
+    message = str(e.value)
+    assert message == "A contents has already been wrapped."
+
