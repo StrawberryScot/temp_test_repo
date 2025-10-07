ea323a64c38b8a5fcb8fa03ed94acada7ba48239
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-10-07 12:52:47 +0100

    tests: added tests for get house details and repaint door

--- tests/test_house.py
+++ tests/test_house.py
@@ -6,3 +6,12 @@ def test_house_attributes():
     assert my_house.floors == 2
     assert my_house.number ==137
     assert my_house.door_color =="white"
+
+def test_get_details():
+    my_house = House(137, "white")
+    assert my_house.get_details == "House number 137 has 2 floors and a white door"
+
+def test_repaint_door():
+    my_house = House(137, "white")
+    my_house.repaint_door("blue")
+    assert my_house.door_color == "blue"
