9ee7a611d22d73def3aecb211ea30b14cba13d5a
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-10-07 12:46:54 +0100

    tests: ajusted first test for all 3 attributes for house class

--- lib/house.py
+++ lib/house.py
@@ -1,3 +1,5 @@
 class House():
-    def __init__(self):
+    def __init__(self, number, door_color):
         self.floors = 2
+        self.number = number
+        self.door_color = door_color
--- tests/test_house.py
+++ tests/test_house.py
@@ -1,6 +1,8 @@
 from lib.house import *
 import pytest
 
-def test_house_instantiates_with_floor_attribute_set_to_2():
-    my_house = House()
+def test_house_attributes():
+    my_house = House(137, "white")
     assert my_house.floors == 2
+    assert my_house.number ==137
+    assert my_house.door_color =="white"
