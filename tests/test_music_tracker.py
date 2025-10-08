774cec795245286dadd5369ba8466f6d2a761a8e
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-10-08 11:40:16 +0100

    tests: adjusted current tests for music tracker to new code

--- tests/test_music_tracker.py
+++ tests/test_music_tracker.py
@@ -3,6 +3,7 @@
 # I want to add tracks I've listened to and see a list of them
 
 from lib.music_tracker import MusicTracker
+from lib.track import Track
 
 def test_music_tracker_instantiates_with_tracklist_attribute():
     mt = MusicTracker()
@@ -10,18 +11,19 @@ def test_music_tracker_instantiates_with_tracklist_attribute():
 
 def test_add_track_adds_track_to_tracklist():
     mt = MusicTracker()
-    mt.add_track("Waterloo by ABBA")
-    assert mt.tracklist == ["Waterloo by ABBA"]
+    mt.add_track("Waterloo", "ABBA")
+    assert mt.tracklist[0].title == "Waterloo"
+    assert mt.tracklist[0].artist == "ABBA"
 
 def test_list_tracks_returns_single_track():
     mt = MusicTracker()
-    mt.add_track("Waterloo by ABBA")
+    mt.add_track("Waterloo", "ABBA")
     assert mt.list_tracks() == ["Waterloo by ABBA"]
 
 def test_list_tracks_returns_two_tracks():
     mt = MusicTracker()
-    mt.add_track("Waterloo by ABBA")
-    mt.add_track("Money Money Money by ABBA")
+    mt.add_track("Waterloo", "ABBA")
+    mt.add_track("Money Money Money", "ABBA")
     assert mt.list_tracks() == [
         "Waterloo by ABBA",
         "Money Money Money by ABBA"
