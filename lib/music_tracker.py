5e064b25a0fe75b006740354004f80f14739ae07
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-10-08 11:32:15 +0100

    feat: added unchecked code for listing track titles and artists

--- lib/music_tracker.py
+++ lib/music_tracker.py
@@ -4,8 +4,12 @@ class MusicTracker:
     def __init__(self):
         self.tracklist = []
 
-    def add_track(self, track):
+    def add_track(self, title, artist):
+        track = Track(title, artist)
         self.tracklist.append(track)
 
     def list_tracks(self):
-        return self.tracklist
+        return [f"{track.title} by {track.artist}" for track in self.tracklist]
+
+    def list_unique_artists(self):
+        return list(set([track.artist for track in self.tracklist]))
