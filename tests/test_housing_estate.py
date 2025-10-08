07f15f0bd358da5ce9d34b9f259a365194d4954f
Benjamin Loveday <benjaminfloveday@hotmail.co.uk>
2025-10-08 11:15:31 +0100

    tests: added get house numbers test

--- tests/test_housing_estate.py
+++ tests/test_housing_estate.py
@@ -8,3 +8,11 @@ def test_add_houses_to_housing_estate():
     housing_estate.houses.append(house1)
     housing_estate.houses.append(house2)
     assert housing_estate.houses == [house1, house2]
+
+def test_get_house_from_housing_estate():
+    house1 = House(137, "white")
+    house2 = House(24, "red")
+    housing_estate = HousingEstate()
+    housing_estate.houses.append(house1)
+    housing_estate.houses.append(house2)
+    assert housing_estate.get_house_numbers() == [137, 24]
